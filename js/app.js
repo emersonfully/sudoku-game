917c7c6fb24824bd4f5e339fac0492e3e4ff4758
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2023-03-21 20:06:17 -0300

    create the input to receive the name

--- js/app.js
+++ js/app.js
@@ -7,10 +7,31 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
     document.querySelector('meta[name="theme-color"').setAttribute('content', isDarkMode ? '#1a1a2e' : '#fff')
 })
 
+const nameInput = document.querySelector('#input-name');
+const startScreen = document.querySelector('#star-screen');
+
+document.querySelector('#btn-play').addEventListener('click', () => {
+    if (nameInput.value.trim().length > 0) {
+        alert('Start Game')
+    } else {
+        nameInput.classList.add('input-err');
+        setTimeout(() => {
+            nameInput.classList.remove('input-err');
+            nameInput.focus();
+        }, 500);
+    }
+})
+
+const getGameInfo = () => JSON.parse(localStorage.getItem('game'))
+
 const init = () => {
     const darkmode = JSON.parse(localStorage.getItem('darkmode'));
     document.body.classList.add(darkmode ? 'dark' : 'light');
-    document.querySelector('meta[name="theme-color"').setAttribute('content', darkmode ? '#1a1a2e' : '#fff')
+    document.querySelector('meta[name="theme-color"').setAttribute('content', darkmode ? '#1a1a2e' : '#fff');
+
+    const game = getGameInfo()
+
+    document.querySelector('#btn-continue').style.display = game ? 'grid' : 'nome';
 
 }
 
