2caa235f9c72fef0fe321a1a0642a69a7b67b760
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2023-03-21 21:30:31 -0300

    create the game grid

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
 
 // Innitial Value
 
+const cells = document.querySelectorAll('.main-grid-cell')
 
 const nameInput = document.querySelector('#input-name');
 const startScreen = document.querySelector('#star-screen');
@@ -16,7 +17,7 @@ const startScreen = document.querySelector('#star-screen');
 let level_index = 0;
 let level = CONSTANT.LEVEL[level_index]
 
-// ---------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
 
 document.querySelector('#btn-level').addEventListener('click', (e) => {
     level_index = level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
@@ -38,6 +39,22 @@ document.querySelector('#btn-play').addEventListener('click', () => {
 
 const getGameInfo = () => JSON.parse(localStorage.getItem('game'))
 
+// add spaces for each 9 cells
+
+const initGameGrid = () => {
+    let index = 0;
+
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        let row = Math.floor(i / CONSTANT.GRID_SIZE);
+        let col = i % CONSTANT.GRID_SIZE;
+        if (row === 2 || row === 5) cells[index].style.marginBottom = '10px';
+        if (col === 2 || col === 5) cells[index].style.marginRight = '10px';
+
+        index++
+    }
+}
+// ----------------------------------------------------------------------------------
+
 const init = () => {
     const darkmode = JSON.parse(localStorage.getItem('darkmode'));
     document.body.classList.add(darkmode ? 'dark' : 'light');
@@ -46,6 +63,8 @@ const init = () => {
     const game = getGameInfo()
 
     document.querySelector('#btn-continue').style.display = game ? 'grid' : 'nome';
+
+    initGameGrid()
 
 }
 
